e77d8038b5f3a0e94fcb00c056f0a13ed8b5c663
İlker Şelimen <ilkerselimen@gmail.com>
2023-05-18 20:42:20 +0300

    junit-assertions

--- src/test/java/Day1/Test04_BeforeAll_AfterAll.java
+++ src/test/java/Day1/Test04_BeforeAll_AfterAll.java
@@ -1,14 +1,15 @@
 package Day1;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class Test04_BeforeAll_AfterAll {
 
     @BeforeAll
-    static void beforeAll(){
+    static void beforeALL(){
         System.out.println("open a file");
     }
 
@@ -19,15 +20,26 @@ public class Test04_BeforeAll_AfterAll {
 
     @Test
     void testWithArrays(){
-        String str = "Unit test with JUnit5";
+
+        String str ="Unit test with JUnit5";
         String[] anlikDizi = str.split(" ");
         String[] beklenenDizi = {"Unit","test","with","JUnit5"};
 
-        System.out.println("testWithArrays() running");
+        System.out.println("testWithArrays() calisti");
 
-        Assertions.assertArrayEquals(beklenenDizi,anlikDizi,"diziler esit degil");
-
+        assertArrayEquals(beklenenDizi, anlikDizi, "Diziler esit degil");
     }
 
+    @Test
+    void testWithArrays2(){
+
+        String str ="Unit test with JUnit5";
+        String[] anlikDizi = str.split(" ");
+        String[] beklenenDizi = {"Unit","test","with","JUnit5"};
+
+        System.out.println("testWithArrays2() calisti");
+
+        assertArrayEquals(beklenenDizi, anlikDizi, "Diziler esit degil");
+    }
 
 }
